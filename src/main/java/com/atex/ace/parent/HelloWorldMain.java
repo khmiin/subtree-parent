6743be2833d82edae60e0395ed2547f9f5c38cb2
hueymiin <huey.miin@atex.com>
2018-04-19 13:49:26 +0800

    Update parent and subproject

--- src/main/java/com/atex/ace/parent/HelloWorldMain.java
+++ src/main/java/com/atex/ace/parent/HelloWorldMain.java
@@ -2,11 +2,13 @@ package com.atex.ace.parent;
 
 
 import com.atex.ace.subproject.ApplicationConfig;
+import com.atex.ace.subproject.ParentConfig;
 
 public class HelloWorldMain {
 
     public static void main(String[] args) {
         System.out.println(ApplicationConfig.VERSION);
+        System.out.println(ParentConfig.NAME);
     }
 
 }
