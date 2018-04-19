fa258eaf84ab9ac42ffff2112eb1fa9f2efe649d
hueymiin <huey.miin@atex.com>
2018-04-19 13:57:00 +0800

    Just parent changes

--- src/main/java/com/atex/ace/parent/HelloWorldMain.java
+++ src/main/java/com/atex/ace/parent/HelloWorldMain.java
@@ -7,6 +7,7 @@ import com.atex.ace.subproject.ParentConfig;
 public class HelloWorldMain {
 
     public static void main(String[] args) {
+        System.out.println("*** " + HelloWorldMain.class.getName());
         System.out.println(ApplicationConfig.VERSION);
         System.out.println(ParentConfig.NAME);
     }
