ab4efaa3b683e80a676d04638f42f1f5dff2ad6e
hueymiin <huey.miin@atex.com>
2018-04-19 11:26:27 +0800

    First commit of parent

---
+++ src/main/java/com/atex/ace/parent/HelloWorldMain.java
@@ -0,0 +1,12 @@
+package com.atex.ace.parent;
+
+
+import com.atex.ace.subproject.ApplicationConfig;
+
+public class HelloWorldMain {
+
+    public static void main(String[] args) {
+        System.out.println(ApplicationConfig.VERSION);
+    }
+
+}
